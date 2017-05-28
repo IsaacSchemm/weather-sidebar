84fad7c12c97a321cfe531142fcdfb9aff9eb995
Isaac Schemm <isaacschemm@gmail.com>
2017-05-28 16:18:05 -0500

    Loas/save using local storage

--- weather-sidebar/main.js
+++ weather-sidebar/main.js
@@ -5,8 +5,8 @@ class WeatherViewModel {
         this.settingsModel = ko.observable(null);
 
         this.twelveHourTime = ko.observable(true);
-        this.latitude = ko.observable(null);
-        this.longitude = ko.observable(null);
+        this.defaultLatitude = ko.observable(null);
+        this.defaultLongitude = ko.observable(null);
         this.useGeolocation = ko.observable(true);
 
         this.error = ko.observable(null);
@@ -17,41 +17,62 @@ class WeatherViewModel {
         this.lastLongitude = null;
 
         this.hourlyForecast = ko.observableArray();
+
+        this.loadSettings();
     }
 
-    async update() {
-        this.updateForecast({
-            latitude: this.latitude(),
-            longitude: this.longitude()
-        });
+    loadSettings(settings) {
+        try {
+            if (settings == null) {
+                if (!window.localStorage) return;
+                let json = localStorage.getItem("settings");
+                if (json == null) return;
+                settings = JSON.parse(json);
+            }
+            this.twelveHourTime(settings.twelveHourTime);
+            this.defaultLatitude(settings.latitude);
+            this.defaultLongitude(settings.longitude);
+            this.useGeolocation(settings.useGeolocation);
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(l => this.updateForecast(l.coords));
+            this.update();
+        } catch (e) {
+            console.error(e);
+            this.error("Could not load settings.");
         }
     }
 
-    async updateForecast(coords) {
+    update() {
+        this.updateLocation({
+            latitude: this.defaultLatitude(),
+            longitude: this.defaultLongitude()
+        });
+
+        if (this.useGeolocation() && navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(l => this.updateLocation(l.coords));
+        }
+    }
+
+    updateLocation(coords) {
+        if (coords.latitude == this.lastLatitude && coords.longitude == this.lastLongitude) {
+            console.log("Same location")
+            return;
+        }
+
+        this.lastLatitude = coords.latitude;
+        this.lastLongitude = coords.longitude;
+        this.updateForecast();
+    }
+
+    async updateForecast() {
         try {
-            if (coords == null) {
-                coords = {
-                    latitude: this.latitude(),
-                    longitude: this.longitude()
-                };
-            }
-
-            if (coords.latitude == null || coords.longitude == null) { 
+            if (this.lastLatitude == null || this.lastLongitude == null) {
+                this.temperature(null);
+                this.description(null);
+                this.hourlyForecast([]);
                 return;
             }
 
-            if (coords.latitude == this.lastLatitude && coords.longitude == this.lastLongitude) {
-                console.log("Same location")
-                return;
-            }
-
-            this.lastLatitude = coords.latitude;
-            this.lastLongitude = coords.longitude;
-
-            let data = await $.getJSON(`proxy.ashx?url=${coords.latitude},${coords.longitude}`);
+            let data = await $.getJSON(`proxy.ashx?url=${this.lastLatitude},${this.lastLongitude}`);
             
             this.temperature(Math.round(data.currently.temperature) + String.fromCodePoint(0xB0));
             this.description(data.currently.summary);
@@ -98,27 +119,38 @@ class WeatherViewModel {
 
     configureSettings() {
         this.settingsModel(new SettingsViewModel(this));
-        this.updateForecast();
     }
 }
 
 class SettingsViewModel {
     constructor(parent) {
+        let numberToString = i => {
+            if (i == null) return "";
+            else return `${i}`;
+        };
         this.parent = parent;
-        for (let name of ["twelveHourTime", "latitude", "longitude", "useGeolocation"]) {
-            this[name] = ko.observable(parent[name]());
-        }
+        this.twelveHourTime = ko.observable(parent.twelveHourTime());
+        this.latitude = ko.observable(numberToString(parent.defaultLatitude()));
+        this.longitude = ko.observable(numberToString(parent.defaultLongitude()));
+        this.useGeolocation = ko.observable(parent.useGeolocation());
     }
 
     saveSettings() {
-        this.parent.twelveHourTime(!!this.twelveHourTime());
-        this.parent.latitude(this.latitude() == "" ? null : +this.latitude());
-        this.parent.longitude(this.longitude() == "" ? null : +this.longitude());
-        this.parent.useGeolocation(!!this.useGeolocation());
-        for (let name of["twelveHourTime", "latitude", "longitude", "useGeolocation"]) {
-            ko.observable(this.parent[name](this[name]()));
+        let normalizeNumber = s => {
+            if (s == null || s == "") return null;
+            else return +s;
+        };
+        let settings = {
+            twelveHourTime: !!this.twelveHourTime(),
+            latitude: normalizeNumber(this.latitude()),
+            longitude: normalizeNumber(this.longitude()),
+            useGeolocation: !!this.useGeolocation()
+        }
+        if (window.localStorage) {
+            localStorage.setItem("settings", JSON.stringify(settings));
         }
         this.parent.settingsModel(null);
+        this.parent.loadSettings(settings);
         this.parent.updateForecast();
     }
 }
