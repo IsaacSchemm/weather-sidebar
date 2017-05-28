0c1f5eb908e25380d3b662078f915aa34673bff1
Isaac Schemm <isaacschemm@gmail.com>
2017-05-28 15:43:38 -0500

    Geolocation support

--- weather-sidebar/main.js
+++ weather-sidebar/main.js
@@ -5,59 +5,46 @@ class WeatherViewModel {
         this.settingsModel = ko.observable(null);
 
         this.twelveHourTime = ko.observable(true);
-        this.latitude = ko.observable(47.6038);
-        this.longitude = ko.observable(-122.3301);
+        this.latitude = ko.observable(null);
+        this.longitude = ko.observable(null);
         this.useGeolocation = ko.observable(true);
 
         this.error = ko.observable(null);
-        this.temperature = ko.observable(70);
-        this.description = ko.observable("Clear");
+        this.temperature = ko.observable(null);
+        this.description = ko.observable(null);
+
+        this.lastLatitude = null;
+        this.lastLongitude = null;
 
         this.hourlyForecast = ko.observableArray();
-
-        this.updateForecast();
     }
 
-    async updateForecast() {
+    async getCoordinates() {
         try {
-            let data = await $.getJSON(`proxy.ashx?url=${this.latitude()},${this.longitude()}`);
-            
-            this.temperature(Math.round(data.currently.temperature));
-            this.description(data.summary);
+            return await new Promise((resolve, reject) => {
+                if (!navigator.geolocation) {
+                    reject();
+                } else {
+                    navigator.geolocation.getCurrentPosition(l => resolve(l.coords), reject);
+                }
+            });
+        } catch (e) {
+            return {
+                latitude: this.latitude(),
+                longitude: this.longitude()
+            };
+        }
+    }
 
-            this.hourlyForecast([]);
-            for (let h of data.hourly.data.slice(0, 18)) {
-                let icon = (() => {
-                    switch (h.icon) {
-                        case "clear-day":
-                        case "clear-night":
-                            return String.fromCodePoint(9728);
-                        case "rain":
-                            return String.fromCodePoint(127783);
-                        case "snow":
-                        case "sleet":
-                            return String.fromCodePoint(127784);
-                        case "wind":
-                            return String.fromCodePoint(128168);
-                        case "fog":
-                            return String.fromCodePoint(127787);
-                        case "cloudy":
-                            return String.fromCodePoint(9729);
-                        case "partly-cloudy-day":
-                        case "partly-cloudy-night":
-                            return String.fromCodePoint(9925);
-                        default:
-                            return "";
-                    }
-                })();
-                icon += String.fromCodePoint(0xFE0F);
-                this.hourlyForecast.push({
-                    time: new Date(h.time * 1000).toLocaleTimeString(),
-                    temp: Math.round(h.temperature || 0) + String.fromCodePoint(0xB0),
-                    icon: icon,
-                    summary: h.summary,
-                    precipProbability: h.precipProbability + "%"
-                });
+    async update() {
+        try {
+            this.updateForecast({
+                latitude: this.latitude(),
+                longitude: this.longitude()
+            });
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(l => this.updateForecast(l.coords));
             }
         } catch (e) {
             console.error(e);
@@ -65,8 +52,70 @@ class WeatherViewModel {
         }
     }
 
+    async updateForecast(coords) {
+        if (coords == null) {
+            coords = {
+                latitude: this.latitude(),
+                longitude: this.longitude()
+            };
+        }
+
+        if (coords.latitude == null || coords.longitude == null) { 
+            return;
+        }
+
+        if (coords.latitude == this.lastLatitude && coords.longitude == this.lastLongitude) {
+            console.log("Same location")
+            return;
+        }
+
+        this.lastLatitude = coords.latitude;
+        this.lastLongitude = coords.longitude;
+
+        let data = await $.getJSON(`proxy.ashx?url=${coords.latitude},${coords.longitude}`);
+            
+        this.temperature(Math.round(data.currently.temperature) + String.fromCodePoint(0xB0));
+        this.description(data.currently.summary);
+
+        this.hourlyForecast([]);
+        for (let h of data.hourly.data.slice(0, 18)) {
+            let icon = (() => {
+                switch (h.icon) {
+                    case "clear-day":
+                    case "clear-night":
+                        return String.fromCodePoint(9728);
+                    case "rain":
+                        return String.fromCodePoint(127783);
+                    case "snow":
+                    case "sleet":
+                        return String.fromCodePoint(127784);
+                    case "wind":
+                        return String.fromCodePoint(128168);
+                    case "fog":
+                        return String.fromCodePoint(127787);
+                    case "cloudy":
+                        return String.fromCodePoint(9729);
+                    case "partly-cloudy-day":
+                    case "partly-cloudy-night":
+                        return String.fromCodePoint(9925);
+                    default:
+                        return "";
+                }
+            })();
+            icon += String.fromCodePoint(0xFE0F);
+            this.hourlyForecast.push({
+                time: new Date(h.time * 1000).toLocaleTimeString(),
+                temp: Math.round(h.temperature || 0) + String.fromCodePoint(0xB0),
+                icon: icon,
+                summary: h.summary,
+                precipProbability: Math.round(h.precipProbability * 100) + "%"
+            });
+        }
+    }
+
     configureSettings() {
         this.settingsModel(new SettingsViewModel(this));
+        this.updateForecast();
     }
 }
 
@@ -79,9 +128,14 @@ class SettingsViewModel {
     }
 
     saveSettings() {
+        this.parent.twelveHourTime(!!this.twelveHourTime());
+        this.parent.latitude(this.latitude() == "" ? null : +this.latitude());
+        this.parent.longitude(this.longitude() == "" ? null : +this.longitude());
+        this.parent.useGeolocation(!!this.useGeolocation());
         for (let name of["twelveHourTime", "latitude", "longitude", "useGeolocation"]) {
             ko.observable(this.parent[name](this[name]()));
         }
         this.parent.settingsModel(null);
+        this.parent.updateForecast();
     }
 }
