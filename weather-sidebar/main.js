090fc7c186fb792382afd4b9bd6f400dfdbe3413
Isaac Schemm <isaacschemm@gmail.com>
2017-05-28 15:58:31 -0500

    cleanup

--- weather-sidebar/main.js
+++ weather-sidebar/main.js
@@ -19,97 +19,80 @@ class WeatherViewModel {
         this.hourlyForecast = ko.observableArray();
     }
 
-    async getCoordinates() {
-        try {
-            return await new Promise((resolve, reject) => {
-                if (!navigator.geolocation) {
-                    reject();
-                } else {
-                    navigator.geolocation.getCurrentPosition(l => resolve(l.coords), reject);
-                }
-            });
-        } catch (e) {
-            return {
-                latitude: this.latitude(),
-                longitude: this.longitude()
-            };
-        }
-    }
-
     async update() {
-        try {
-            this.updateForecast({
-                latitude: this.latitude(),
-                longitude: this.longitude()
-            });
+        this.updateForecast({
+            latitude: this.latitude(),
+            longitude: this.longitude()
+        });
 
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(l => this.updateForecast(l.coords));
-            }
-        } catch (e) {
-            console.error(e);
-            this.error(e.message || e.responseText || "An error occured.");
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(l => this.updateForecast(l.coords));
         }
     }
 
     async updateForecast(coords) {
-        if (coords == null) {
-            coords = {
-                latitude: this.latitude(),
-                longitude: this.longitude()
-            };
-        }
+        try {
+            if (coords == null) {
+                coords = {
+                    latitude: this.latitude(),
+                    longitude: this.longitude()
+                };
+            }
 
-        if (coords.latitude == null || coords.longitude == null) { 
-            return;
-        }
+            if (coords.latitude == null || coords.longitude == null) { 
+                return;
+            }
 
-        if (coords.latitude == this.lastLatitude && coords.longitude == this.lastLongitude) {
-            console.log("Same location")
-            return;
-        }
+            if (coords.latitude == this.lastLatitude && coords.longitude == this.lastLongitude) {
+                console.log("Same location")
+                return;
+            }
 
-        this.lastLatitude = coords.latitude;
-        this.lastLongitude = coords.longitude;
+            this.lastLatitude = coords.latitude;
+            this.lastLongitude = coords.longitude;
 
-        let data = await $.getJSON(`proxy.ashx?url=${coords.latitude},${coords.longitude}`);
+            let data = await $.getJSON(`proxy.ashx?url=${coords.latitude},${coords.longitude}`);
             
-        this.temperature(Math.round(data.currently.temperature) + String.fromCodePoint(0xB0));
-        this.description(data.currently.summary);
+            this.temperature(Math.round(data.currently.temperature) + String.fromCodePoint(0xB0));
+            this.description(data.currently.summary);
 
-        this.hourlyForecast([]);
-        for (let h of data.hourly.data.slice(0, 18)) {
-            let icon = (() => {
-                switch (h.icon) {
-                    case "clear-day":
-                    case "clear-night":
-                        return String.fromCodePoint(9728);
-                    case "rain":
-                        return String.fromCodePoint(127783);
-                    case "snow":
-                    case "sleet":
-                        return String.fromCodePoint(127784);
-                    case "wind":
-                        return String.fromCodePoint(128168);
-                    case "fog":
-                        return String.fromCodePoint(127787);
-                    case "cloudy":
-                        return String.fromCodePoint(9729);
-                    case "partly-cloudy-day":
-                    case "partly-cloudy-night":
-                        return String.fromCodePoint(9925);
-                    default:
-                        return "";
-                }
-            })();
-            icon += String.fromCodePoint(0xFE0F);
-            this.hourlyForecast.push({
-                time: new Date(h.time * 1000).toLocaleTimeString(),
-                temp: Math.round(h.temperature || 0) + String.fromCodePoint(0xB0),
-                icon: icon,
-                summary: h.summary,
-                precipProbability: Math.round(h.precipProbability * 100) + "%"
-            });
+            this.hourlyForecast([]);
+            for (let h of data.hourly.data.slice(0, 18)) {
+                let icon = (() => {
+                    switch (h.icon) {
+                        case "clear-day":
+                        case "clear-night":
+                            return String.fromCodePoint(9728);
+                        case "rain":
+                            return String.fromCodePoint(127783);
+                        case "snow":
+                        case "sleet":
+                            return String.fromCodePoint(127784);
+                        case "wind":
+                            return String.fromCodePoint(128168);
+                        case "fog":
+                            return String.fromCodePoint(127787);
+                        case "cloudy":
+                            return String.fromCodePoint(9729);
+                        case "partly-cloudy-day":
+                        case "partly-cloudy-night":
+                            return String.fromCodePoint(9925);
+                        default:
+                            return "";
+                    }
+                })();
+                icon += String.fromCodePoint(0xFE0F);
+                this.hourlyForecast.push({
+                    time: new Date(h.time * 1000).toLocaleTimeString(),
+                    temp: Math.round(h.temperature || 0) + String.fromCodePoint(0xB0),
+                    icon: icon,
+                    summary: h.summary,
+                    precipProbability: Math.round(h.precipProbability * 100) + "%"
+                });
+            }
+        } catch (e) {
+            console.error(e);
+            this.error(e.message || e.responseText || "An error occured.");
         }
     }
 
