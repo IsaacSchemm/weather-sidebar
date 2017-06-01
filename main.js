0c64f15b2acb6278abd41d7e5a06c73adce236d8
Isaac Schemm <isaacschemm@gmail.com>
2017-06-01 12:13:10 -0500

    Add ability to use Twitter emoji

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 ﻿"use strict";
 
-// Returns a Promise that resolves to the current coordinates or gets
-// rejected if the current position cannot be determined. Coordinates are
-// limited in precision to avoid unnecessary weather lookups due to small
-// variations.
 function getCurrentCoords() {
+    // Returns a Promise that resolves to the current coordinates or gets
+    // rejected if the current position cannot be determined. Coordinates are
+    // limited in precision to avoid unnecessary weather lookups due to small
+    // variations.
     return new Promise((resolve, reject) => {
         if (!navigator.geolocation) {
             reject();
@@ -14,6 +14,16 @@ function getCurrentCoords() {
             latitude: +loc.coords.latitude.toFixed(3),
             longitude: +loc.coords.longitude.toFixed(3)
         }), reject);
+    });
+}
+
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const s = document.createElement('script');
+        s.src = src;
+        s.onload = resolve;
+        s.onerror = reject;
+        document.head.appendChild(s);
     });
 }
 
@@ -29,6 +39,7 @@ class WeatherViewModel {
         this.defaultLongitude = ko.observable(null);
         this.useGeolocation = ko.observable(true);
         this.theme = ko.observable("compact-light");
+        this.useTwitterEmoji = ko.observable(/Windows NT [456]\..*/.test(navigator.userAgent));
 
         this.error = ko.observable(null);
 
@@ -81,7 +92,7 @@ class WeatherViewModel {
     // Loads and applies new settings from HTML local storage.
     loadSettings() {
         try {
-            let clockSettingsChanged = false;
+            let displaySettingsChanged = false;
             let json = localStorage.getItem("settings");
             if (json != null) {
                 let settings = JSON.parse(json);
@@ -89,24 +100,29 @@ class WeatherViewModel {
                 // Clock settings
                 if (this.twelveHourTime() != settings.twelveHourTime) {
                     this.twelveHourTime(settings.twelveHourTime);
-                    clockSettingsChanged = true;
+                    displaySettingsChanged = true;
                 }
                 if (this.useLocationTime() != settings.useLocationTime) {
                     this.useLocationTime(settings.useLocationTime);
-                    clockSettingsChanged = true;
+                    displaySettingsChanged = true;
+                }
+                if (this.useTwitterEmoji() != settings.useTwitterEmoji) {
+                    this.useTwitterEmoji(settings.useTwitterEmoji);
+                    displaySettingsChanged = true;
                 }
 
                 // Location settings
                 this.defaultLatitude(settings.latitude);
                 this.defaultLongitude(settings.longitude);
                 this.useGeolocation(settings.useGeolocation);
+                this.useTwitterEmoji(settings.useTwitterEmoji);
 
                 this.theme(settings.theme || "compact-light");
             }
 
             // If one of the time display settings changed, we'll want to refresh.
             // Otherwise, we only need to refresh if our location has changed.
-            this.update(clockSettingsChanged ? true : false);
+            this.update(displaySettingsChanged ? true : false);
         } catch (e) {
             console.error(e);
             this.error("Could not load settings.");
@@ -144,6 +160,18 @@ class WeatherViewModel {
         this.currentLatitude(coords.latitude);
         this.currentLongitude(coords.longitude);
         this.updateForecast();
+    }
+
+    async emojiStr(codePoint) {
+        const str = String.fromCodePoint(codePoint) + String.fromCodePoint(0xFE0F);
+        if (this.useTwitterEmoji()) {
+            if (!window.twemoji) {
+                await loadScript("https://twemoji.maxcdn.com/2/twemoji.min.js?2.3.0");
+            }
+            return twemoji.parse(str);
+        } else {
+            return str;
+        }
     }
 
     // Updates the forecast information.
@@ -190,38 +218,36 @@ class WeatherViewModel {
             }
 
             this.time("Last updated: " + moment.tz(data.currently.time * 1000, timezone).format(format + " z"));
-            this.temperature(Math.round(data.currently.temperature) + String.fromCodePoint(0xB0));
+            this.temperature(Math.round(data.currently.temperature) + String.fromCharCode(0xB0));
             this.description(data.currently.summary);
 
             this.hourlyForecast([]);
             for (let h of data.hourly.data.slice(0, 12)) {
                 // Use an anonymous function to map an icon name to a Unicode icon
-                let icon = (() => {
+                const codePoint = (() => {
                     switch (h.icon) {
                         case "clear-day":
                         case "clear-night":
-                            return String.fromCodePoint(9728);
+                            return 9728;
                         case "rain":
-                            return String.fromCodePoint(127783);
+                            return 127783;
                         case "snow":
                         case "sleet":
-                            return String.fromCodePoint(127784);
+                            return 127784;
                         case "wind":
-                            return String.fromCodePoint(128168);
+                            return 128168;
                         case "fog":
-                            return String.fromCodePoint(127787);
+                            return 127787;
                         case "cloudy":
-                            return String.fromCodePoint(9729);
+                            return 9729;
                         case "partly-cloudy-day":
                         case "partly-cloudy-night":
-                            return String.fromCodePoint(9925);
+                            return 9925;
                         default:
                             return "";
-                }
+                    }
                 })();
-                
-                // Display as emoji if possible
-                icon += String.fromCodePoint(0xFE0F);
+                const icon = await this.emojiStr(codePoint);
 
                 this.hourlyForecast.push({
                     time: moment.tz(h.time * 1000, timezone).format(format),
@@ -257,6 +283,7 @@ class SettingsViewModel {
         this.latitude = ko.observable(numberToString(parent.defaultLatitude()));
         this.longitude = ko.observable(numberToString(parent.defaultLongitude()));
         this.useGeolocation = ko.observable(parent.useGeolocation());
+        this.useTwitterEmoji = ko.observable(parent.useTwitterEmoji());
 
         // By using the same observable, the theme will update instantly
         this.theme = parent.theme;
@@ -300,6 +327,7 @@ class SettingsViewModel {
             latitude: normalizeNumber(this.latitude()),
             longitude: normalizeNumber(this.longitude()),
             useGeolocation: !!this.useGeolocation(),
+            useTwitterEmoji: !!this.useTwitterEmoji(),
             theme: this.theme()
         }
         localStorage.setItem("settings", JSON.stringify(settings));
